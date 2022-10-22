9363ba917fa999bc0f3487b927335b574bfcb019
susmitha <susmithareddychinthala024@gmail.com>
2022-10-22 20:36:51 +0530

    Added: Gambler Starts the Game

---
+++ src/com/gamblingsimulation/GamblerStartsTheGame.java
@@ -0,0 +1,13 @@
+package com.gamblingsimulation;
+//    / UC1 - As a Gambler, would start with a stake of $100 every day and bet $1 every game.
+
+public class GamblerStartsTheGame {
+
+    public static final int STAKE = 100;
+    public static final int BET_VALUE = 1;
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Gambling Simulation Program");
+    }
+}
+
